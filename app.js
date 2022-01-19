378b7277be8ee6e7b205359d36ae9cb401b5a7c1
Facundo Jaramillo <facu.jaramillo@gmail.com>
2022-01-18 22:27:41 -0300

    fix bug

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const express = require ("express");
-
+const express = require('express');
 const app = express();
-
-app.use(express.static("public"))
-
-app.get("/home", (req, res)=>{
-    res.sendFile(__dirname +"/views/home.html");
-})
+app.use(express.static('public'));
 
 
-app.listen (process.env.PORT || 3000, ()=>{
-    console.log("estás en el puerto 3000")
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
 });
 
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
